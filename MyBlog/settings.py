aebdb5fa34f432299ae6e1fea55b4dc2872fce6b
eric4130 <synthpad413@gmail.com>
2021-12-28 17:54:45 -0500

    secret key fix

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rr&vt80^pnva&a_cd4ff0kooyp!-l3@#i-z#b1z_$zn0#t(fl^'
+SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
